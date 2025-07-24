e31b0c5b6427fb66f02af7101219e7ed1dd9200c
SabriRamadhani <sabriramadhani886@gmail.com>
2025-07-24 21:20:37 +0700

    fix: new map, minimap and passenger seats

--- Assets/Scripts/MinimapMovement.cs
+++ Assets/Scripts/MinimapMovement.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class MinimapFollow : MonoBehaviour
 {
     public Transform target;
-    public Vector3 offset = new Vector3(0f, 44f, 0f);
+    public Vector3 offset = new Vector3(0f, 28f, 0f);
 
     void LateUpdate()
     {
